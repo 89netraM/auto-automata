8932292f377a5d21f5d71fee4efe3df5e6e20abe
Mårten Åsberg <marten.asberg@outlook.com>
2021-01-26 09:23:28 +0100

    Validator validates start and accepting

--- src/validate.ts
+++ src/validate.ts
@@ -13,23 +13,40 @@ export interface ValidatorError {
  */
 export function validate(g: Graph, l: ReadonlySet<string>): boolean | Array<ValidatorError> {
 	const errors = new Array<ValidatorError>();
+
+	if (!(g.start in g.states)) {
+		errors.push({
+			path: ["start"],
+			error: `no state named "${g.start}" exists`
+		});
+	}
+	for (const targetStateName of g.accepting) {
+		if (!(targetStateName in g.states)) {
+			errors.push({
+				path: ["accepting"],
+				error: `no state named "${targetStateName}" exists`
+			});
+		}
+	}
+
 	for (const stateName in g.states) {
 		for (const symbol in g.states[stateName]) {
 			if (!l.has(symbol) && symbol !== Graph.Epsilon) {
 				errors.push({
-					path: [stateName],
+					path: ["states", stateName],
 					error: `"${symbol}" is not part of the language`
 				});
 			}
 			for (const targetStateName of g.states[stateName][symbol]) {
 				if (!(targetStateName in g.states)) {
 					errors.push({
-						path: [stateName, symbol],
+						path: ["states", stateName, symbol],
 						error: `no state named "${targetStateName}" exists`
 					});
 				}
 			}
 		}
 	}
+
 	return errors.length === 0 || errors;
 }
