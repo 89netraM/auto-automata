ac297d3a1d0da8fafb27a1150fbbd07e688090be
Mårten Åsberg <marten.asberg@outlook.com>
2021-02-01 10:22:55 +0100

    More Sequence simplification rules

--- src/RegularExpressions/Sequence.ts
+++ src/RegularExpressions/Sequence.ts
@@ -1,3 +1,4 @@
+import { Alternative } from "./Alternative";
 import { Empty } from "./Empty";
 import { Nil } from "./Nil";
 import { Reference } from "./Reference";
@@ -7,11 +8,33 @@ import { Star } from "./Star";
 export class Sequence extends RegularExpression {
 	public static readonly Character: string = "";
 
+	public readonly left: RegularExpression;
+	public readonly right: RegularExpression;
+
 	public constructor(
-		public readonly left: RegularExpression,
-		public readonly right: RegularExpression,
-	) {
+		left: RegularExpression,
+		right: RegularExpression,
+	);
+	public constructor(alternatives: ReadonlyArray<RegularExpression>);
+	public constructor(a: RegularExpression | ReadonlyArray<RegularExpression>, b?: RegularExpression) {
 		super();
+		if (a instanceof RegularExpression) {
+			this.left = a;
+			this.right = b;
+		}
+		else {
+			if (a.length < 2) {
+				throw new Error("empty or only element in sequence");
+			}
+			else if (a.length === 2) {
+				this.left = a[0];
+				this.right = a[1];
+			}
+			else {
+				this.left = new Sequence(a.slice(0, a.length - 1));
+				this.right = a[a.length - 1];
+			}
+		}
 	}
 
 	public flat(): Array<RegularExpression> {
@@ -59,6 +82,22 @@ export class Sequence extends RegularExpression {
 		// e*e = ee*
 		else if (left instanceof Star && left.exp.equals(right)) {
 			return new Sequence(right, left);
+		}
+		// (e₁*e₂)*e₁* = (e₁ + e₂)*
+		else if (left instanceof Star && left.exp instanceof Sequence && left.exp.flat()[0].equals(right) && right instanceof Star) {
+			const elements = left.exp.flat().slice(1);
+			let rightRight: RegularExpression;
+			if (elements.length === 1) {
+				rightRight = elements[0];
+			}
+			else {
+				rightRight = new Sequence(elements);
+			}
+			return new Star(new Alternative(right.exp, rightRight)).simplify();
+		}
+		// (e₁e₂)*e₁ = e₁(e₂e₁)*
+		else if (left instanceof Star && left.exp instanceof Sequence && left.exp.flat()[0].equals(right)) {
+			return new Sequence(right, new Star(new Sequence([...left.exp.flat().slice(1), right]))).simplify();
 		}
 		else {
 			return new Sequence(left, right);
