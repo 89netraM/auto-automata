3b2e7352252d83f57a8eba2c2fd62c68d93b49ad
Mårten Åsberg <marten.asberg@outlook.com>
2021-02-02 19:10:52 +0100

    Parsing and formatting the "official" course Automata file format

--- src/formatter.ts
+++ src/formatter.ts
@@ -2,9 +2,13 @@ import { Automata } from "./Automata";
 import { Graph } from "./Graph";
 
 export function formatTable(a: Readonly<Automata>): string {
-	const transitions = Object.keys(a.states).some(k => Graph.Epsilon in a.states[k]) ? [Graph.Epsilon, ...a.alphabet] : [...a.alphabet];
+	const transitions = Object.values(a.states).some(s => Graph.Epsilon in s) ? [Graph.Epsilon, ...a.alphabet] : [...a.alphabet];
+	const isDFA = Object.values(a.states).every(s =>
+		!Object.keys(s).some(k => k === Graph.Epsilon) &&
+		Object.values(s).every(set => set.size === 1)
+	);
 	return [
-		`\t\t${transitions.join("\t")}`,
+		`\t${transitions.join("\t")}`,
 		...Object.entries(a.states)
 			.sort()
 			.map(
@@ -14,7 +18,7 @@ export function formatTable(a: Readonly<Automata>): string {
 					key + "\t" +
 					transitions.map(
 						l => l in value && (value[l].size > 1 || !value[l].has(Graph.Empty)) ?
-							value[l].size > 1 ? `{${[...value[l]].join(", ")}}` : [...value[l]][0] :
+							!isDFA ? `{${[...value[l]].join(", ")}}` : [...value[l]][0] :
 							Graph.Empty
 					).join("\t")
 			)
--- src/parser.ts
+++ src/parser.ts
@@ -1,30 +1,62 @@
 import { Automata } from "./Automata";
 import { Graph } from "./Graph";
 
+const comment = "#";
+
 export function parseTable(text: string): Automata | null {
-	const [alphabetList, ...stateTransitions] = text.trim().split("\n").map(l => l.trim().split(/\t| {2,}/));
+	const [alphabetLine, ...stateTransitions] = text
+		.trim()
+		.split(/\r?\n/)
+		.map(l => l.substring(0, (l.indexOf(comment) + 1 || l.length + 1) - 1).trim())
+		.filter(l => l.length > 0);
+	const alphabetList = alphabetLine.split(/\s+/);
 	let starting: string;
 	const accepting = new Set<string>();
 	const states: Graph = {};
 
-	for (const [state, ...transitions] of stateTransitions) {
-		const stateName = state.match(/[^\s]*$/)[0];
+	for (const line of stateTransitions) {
+		const [, arrow, star, stateName, transitions] = line.match(/^(→|->)? ?(\*)? ?(.+?)\s(.*)/);
 
-		if (starting == null && /^\*?→/.test(state)) {
+		if (starting == null && arrow != null) {
 			starting = stateName;
 		}
-		if (/^→?\*/.test(state)) {
+		if (star != null) {
 			accepting.add(stateName);
 		}
 
 		states[stateName] = {};
+		let character = 0;
 		for (let i = 0; i < transitions.length; i++) {
-			const transition = transitions[i];
-			if (transition.startsWith("{") && transition.endsWith("}")) {
-				states[stateName][alphabetList[i]] = new Set(transition.substring(1, transition.length - 1).split(",").map(s => s.trim()));
+			const readStateName = (): string => {
+				let targetState = "";
+				for (; i < transitions.length && !/\s|\{|\}|,/.test(transitions[i]); i++) {
+					targetState += transitions[i];
+				}
+				return targetState.length > 0 ? targetState : null;
+			};
+
+			if (/\s/.test(transitions[i])) {
+				continue;
 			}
-			else if (transition !== Graph.Empty) {
-				states[stateName][alphabetList[i]] = new Set([ transition ]);
+			else if (transitions[i] === "{") {
+				const targetStates = new Set<string>();
+				while (i + 1 < transitions.length && transitions[i] !== "}") {
+					i++;
+					const targetState = readStateName();
+					if (targetState != null) {
+						targetStates.add(targetState);
+					}
+				}
+				states[stateName][alphabetList[character++]] = targetStates;
+			}
+			else {
+				const targetState = readStateName();
+				if (targetState === Graph.Empty) {
+					states[stateName][alphabetList[character++]] = new Set<string>();
+				}
+				else if (targetState != null) {
+					states[stateName][alphabetList[character++]] = new Set<string>([ targetState ]);
+				}
 			}
 		}
 	}
