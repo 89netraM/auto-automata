7b3894a9926ab3d2f3fae4a54c3eaa12d10e16e6
Mårten Åsberg <marten.asberg@outlook.com>
2021-02-03 16:45:38 +0100

    Name generators instead of actual names
    They make it easier 🥴

--- src/from.ts
+++ src/from.ts
@@ -4,92 +4,126 @@ import { Graph } from "./Graph";
 import { Nil } from "./RegularExpressions/Nil";
 import { Empty } from "./RegularExpressions/Empty";
 
-export function fromRegularExpression(regularExpression: RegularExpression, alphabet: Set<string>): Automata {
-	let nextStateId = 0;
-	const getNextState = (): string => {
-		const bigString = (nextStateId++).toString();
-		let outString = "s";
+class StateNameGenerator {
+	public static generateName(number: number): string {
+		const bigString = number.toString();
+		let nameString = "s";
 		for (let i = 0; i < bigString.length; i++) {
-			outString += String.fromCodePoint(bigString.codePointAt(i) + 0x2050);
+			nameString += String.fromCodePoint(bigString.codePointAt(i) + 0x2050);
 		}
-		return outString;
+		return nameString;
+	}
+
+	public constructor(
+		public number: number,
+	) { }
+
+	public toString(): string {
+		return StateNameGenerator.generateName(this.number);
+	}
+}
+
+export function fromRegularExpression(regularExpression: RegularExpression, alphabet: Set<string>): Automata {
+	const states: Graph = { };
+
+	const stateNames = new Array<StateNameGenerator>();
+	const getStateAfter = (previous: StateNameGenerator): StateNameGenerator => {
+		const next = new StateNameGenerator(previous.number + 1);
+		for (const generator of stateNames) {
+			if (generator.number > previous.number) {
+				generator.number++;
+				const currentName = generator.toString();
+				const nextName = StateNameGenerator.generateName(generator.number + 1);
+				for (const state of Object.values(states).filter(s => s != null)) {
+					for (const set of Object.values(state).filter(s => s != null)) {
+						if (set.has(currentName)) {
+							set.delete(currentName);
+							set.add(nextName);
+						}
+					}
+				}
+				states[nextName] = states[currentName];
+				delete states[currentName];
+			}
+		}
+		stateNames.push(next);
+		return next;
 	};
 
-	const states: Graph = { };
-	const addToStates = (start: string, exp: RegularExpression): string => {
+	const addToStates = (start: StateNameGenerator, exp: RegularExpression): StateNameGenerator => {
 		if (exp instanceof Alternative) {
-			const aStart = getNextState();
+			const aStart = getStateAfter(start);
+			const bStart = getStateAfter(aStart);
 			const aEnd = addToStates(aStart, exp.left);
-			const bStart = getNextState();
 			const bEnd = addToStates(bStart, exp.right);
-			states[start] = {
-				[Graph.Epsilon]: new Set([ aStart, bStart ]),
+			states[start.toString()] = {
+				[Graph.Epsilon]: new Set([ aStart.toString(), bStart.toString() ]),
 			};
-			const end = getNextState();
-			states[aEnd] = {
-				[Graph.Epsilon]: new Set([ end ]),
+			const end = getStateAfter(bEnd);
+			states[aEnd.toString()] = {
+				[Graph.Epsilon]: new Set([ end.toString() ]),
 			};
-			states[bEnd] = {
-				[Graph.Epsilon]: new Set([ end ]),
+			states[bEnd.toString()] = {
+				[Graph.Epsilon]: new Set([ end.toString() ]),
 			};
 			return end;
 		}
 		else if (exp instanceof Sequence) {
 			const aEnd = addToStates(start, exp.left);
-			const bStart = getNextState();
-			states[aEnd] = {
-				[Graph.Epsilon]: new Set([ bStart ]),
+			const bStart = getStateAfter(aEnd);
+			states[aEnd.toString()] = {
+				[Graph.Epsilon]: new Set([ bStart.toString() ]),
 			};
 			return addToStates(bStart, exp.right);
 		}
 		else if (exp instanceof Star) {
-			const innerStart = getNextState();
+			const innerStart = getStateAfter(start);
+			const end = getStateAfter(innerStart);
 			const innerEnd = addToStates(innerStart, exp.exp);
-			const end = getNextState();
-			states[start] = {
-				[Graph.Epsilon]: new Set([ innerStart, end ]),
+			states[start.toString()] = {
+				[Graph.Epsilon]: new Set([ innerStart.toString(), end.toString() ]),
 			};
-			states[innerEnd] = {
-				[Graph.Epsilon]: new Set([ innerStart, end ]),
+			states[innerEnd.toString()] = {
+				[Graph.Epsilon]: new Set([ innerStart.toString(), end.toString() ]),
 			};
 			return end;
 		}
 		else if (exp instanceof Empty) {
-			const end = getNextState();
-			states[start] = { };
+			const end = getStateAfter(start);
+			states[start.toString()] = { };
 			return end;
 		}
 		else if (exp instanceof Nil) {
-			const end = getNextState();
-			states[start] = {
-				[Graph.Epsilon]: new Set([ end ]),
+			const end = getStateAfter(start);
+			states[start.toString()] = {
+				[Graph.Epsilon]: new Set([ end.toString() ]),
 			};
 			return end;
 		}
 		else if (exp instanceof Symbol) {
-			const end = getNextState();
-			states[start] = {
-				[exp.symbol]: new Set([ end ]),
+			const end = getStateAfter(start);
+			states[start.toString()] = {
+				[exp.symbol]: new Set([ end.toString() ]),
 			};
 			return end;
 		}
 		else if (exp instanceof Reference) {
-			const end = getNextState();
-			states[start] = {
-				[exp.name]: new Set([ end ]),
+			const end = getStateAfter(start);
+			states[start.toString()] = {
+				[exp.name]: new Set([ end.toString() ]),
 			};
 			return end;
 		}
 	};
 
-	const starting = getNextState();
+	const starting = new StateNameGenerator(0);
 	const accepting = addToStates(starting, regularExpression);
-	states[accepting] = { };
+	states[accepting.toString()] = { };
 
 	return {
 		alphabet,
-		accepting: new Set<string>([accepting]),
-		starting,
+		accepting: new Set<string>([ accepting.toString() ]),
+		starting: starting.toString(),
 		states,
 	};
 }
