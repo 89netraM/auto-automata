5ae0b8b455cb2de39e736c4608444e760b51da43
Mårten Åsberg <marten.asberg@outlook.com>
2021-03-12 07:57:37 +0100

    Web: Copy State Equivalence Tables as LaTeX

--- app/src/components/StateEquivalenceTable.tsx
+++ app/src/components/StateEquivalenceTable.tsx
@@ -5,33 +5,65 @@ export interface Properties {
 	table: StateEquivalenceTableType;
 }
 
+enum CopyType {
+	LaTeX,
+}
+
 export function StateEquivalenceTable(props: Properties): JSX.Element {
+	async function copyAs(button: HTMLButtonElement, type: CopyType): Promise<void> {
+		button.classList.remove("success", "fail");
+		try {
+			let text: string;
+			switch (type) {
+				case CopyType.LaTeX:
+					text = StateEquivalenceTableType.formatLaTeX(props.table);
+					break;
+				default:
+					throw null;
+			}
+			await navigator.clipboard.writeText(text);
+			button.classList.add("success");
+		}
+		catch {
+			setTimeout(() => button.classList.add("fail"));
+		}
+	}
+
 	return (
-		<table>
-			<tbody>
-				<tr>
-					<th></th>
+		<div>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						{
+							Object.keys(Object.values(props.table)[0]).map(k =>
+								<th key={k}>{k}</th>
+							)
+						}
+					</tr>
 					{
-						Object.keys(Object.values(props.table)[0]).map(k =>
-							<th key={k}>{k}</th>
+						Object.entries(props.table).map(([n, states]) =>
+							<tr key={n}>
+								<th>{n}</th>
+								{
+									Object.entries(states).map(([n, v]) =>
+										<td key={n}>
+											{v ? "✔️" : "❌"}
+										</td>
+									)
+								}
+							</tr>
 						)
 					}
-				</tr>
-				{
-					Object.entries(props.table).map(([n, states]) =>
-						<tr key={n}>
-							<th>{n}</th>
-							{
-								Object.entries(states).map(([n, v]) =>
-									<td key={n}>
-										{v ? "✔️" : "❌"}
-									</td>
-								)
-							}
-						</tr>
-					)
-				}
-			</tbody>
-		</table>
+				</tbody>
+			</table>
+			<div className="action-buttons">
+				<button
+					data-icon="📄"
+					onClick={e => copyAs(e.currentTarget, CopyType.LaTeX)}
+					title="Copy this table as LaTeX"
+				>LaTeX</button>
+			</div>
+		</div>
 	);
 }
